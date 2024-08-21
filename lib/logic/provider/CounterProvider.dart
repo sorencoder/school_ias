273ee7feefba9d84b35f4c88fe68692b14b24025
sorencoder <69846992+sorencoder@users.noreply.github.com>
2024-08-21 23:12:44 +0530

    Delete lib directory

--- lib/logic/provider/CounterProvider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CounterProvier extends ChangeNotifier {
-  int counter = 0;
-  void increment() {
-    counter++;
-    notifyListeners();
-  }
-}
--- lib/logic/provider/passwordvisibiltyprovider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class PasswordVisiblityProvider extends ChangeNotifier {
-  bool isvisible = true;
-  void setVisiblity() {
-    isvisible = !isvisible;
-    notifyListeners();
-  }
-}
